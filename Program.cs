bdfce4606d1c35d5149248a78ef34aad59c65046
Джанклод Вандам <93411756+Ggyuiiiiiiiijhtcc@users.noreply.github.com>
2023-06-16 11:10:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+namespace Localizate
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
